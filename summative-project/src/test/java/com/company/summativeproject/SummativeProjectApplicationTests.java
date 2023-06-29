07847944945f5a3be41b2d65483d5e1133a68492
unknown <chrisrojas514@gmail.com>
2023-06-29 19:02:51 -0400

    Added base project created from spring intitializr - Dependencies: Spring Web

---
+++ summative-project/src/test/java/com/company/summativeproject/SummativeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.summativeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummativeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
