7dfefb41880a7852bff86cfd4474aa94a60231df
jms2wyw <jms2wyw@virginia.edu>
2023-07-06 11:42:43 -0400

    word-api done

---
+++ summative-project/src/test/java/com/company/summativeproject/controller/WordApiControllerTest.java
@@ -0,0 +1,43 @@
+package com.company.summativeproject.controller;
+import com.company.summativeproject.models.Definition;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(WordApiController.class)
+public class WordApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private List<Definition> wordList;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+    @Test
+    public void shouldReturnRandomWordInList() throws Exception {
+
+        // ARRANGE
+        // Convert Java object to JSON
+        String outputJson = mapper.writeValueAsString(wordList);
+
+        // ACT
+        mockMvc.perform(get("/word"))                // Perform the GET request
+                .andDo(print())                          // Print results to console
+                .andExpect(status().isOk());              // ASSERT (status code is 200)
+    }
+}
+
