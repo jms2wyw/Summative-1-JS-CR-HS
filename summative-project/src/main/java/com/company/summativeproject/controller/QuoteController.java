a4e575a8b543f70782f098cab92091e9fa00fb5a
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-07-10 18:11:53 -0700

    main: remove unnecessary import

--- summative-project/src/main/java/com/company/summativeproject/controller/QuoteController.java
+++ summative-project/src/main/java/com/company/summativeproject/controller/QuoteController.java
@@ -1,7 +1,6 @@
 package com.company.summativeproject.controller;
 
 import com.company.summativeproject.model.Quote;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
